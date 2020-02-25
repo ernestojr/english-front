da7d1ec387c6cce23aa8ddab719098d37b65de1c
Ernesto Rojas <ernesto20145@gmail.com>
2020-02-25 02:09:09 -0300

    Initial commit.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid'
+import {
+  Container,
+  Row,
+  Col,
+  Button,
+  Form,
+  FormGroup,
+  Input,
+  ListGroup,
+  ListGroupItem,
+} from 'reactstrap';
+
+const onSubmit = ({lines, text, type}, { setNewLine, cleanForm }) => (e) => {
+  e.preventDefault();
+  const l = [...lines];
+  l.push({ text, type });
+  setNewLine(l);
+  cleanForm();
+}
+
+const cleanForm = ({ setText, setType }) => () => {
+  setText('');
+  setType('simple');
+};
 
 function App() {
+  const [lines, setNewLine] = useState([]);
+  const [text, setText] = useState('');
+  const [type, setType] = useState('simple');
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Row>
+        <Col>
+          <h1>Englis Practice</h1>
+          <Form onSubmit={onSubmit({lines, text, type}, { setNewLine, cleanForm: cleanForm({ setText, setType }) })} inline>
+            <FormGroup>
+              <Input
+                className="mr-2"
+                type="text"
+                name="text"
+                id="text"
+                placeholder="Text"
+                value={text}
+                required
+                onChange={e => setText(e.target.value)}
+              />
+            </FormGroup>
+            <FormGroup>
+              <Input
+                className="mr-2"
+                type="select"
+                name="type"
+                id="type"
+                placeholder="Type"
+                value={type}
+                required
+                onChange={e => setType(e.target.value)}
+              >
+                <option value="simple">Simple</option>
+                <option value="request">Request</option>
+                <option value="response">Response</option>
+              </Input>
+            </FormGroup>
+            <Button type="submit">Submit</Button>
+          </Form>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <h1>Lines list</h1>
+          <ListGroup>
+            {
+              lines.map(item => <ListGroupItem key={uuidv4()}>{`${item.text} (${item.type})`}</ListGroupItem>)
+            }
+          </ListGroup>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
