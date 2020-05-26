7ea9a8072e5ad648171eb3824c50e019fc3f7e2e
Ernesto Rojas <ernesto20145@gmail.com>
2020-05-26 06:57:16 -0400

    Added resposive style.

--- src/components/HeaderPage.jsx
+++ src/components/HeaderPage.jsx
@@ -1,26 +1,33 @@
 import React from 'react';
 import {
+  Row, Col,
   Button,
 } from 'reactstrap';
 
 export default (props) => (
-  <div className="clearfix">
-    <h1 className="float-left">{props.headerText}</h1>
-    {
-      props.onButtonClickNew &&
-      <Button
-        className="float-right"
-        color="primary"
-        type="button"
-        onClick={props.onButtonClickNew}>{props.buttonTextNew}</Button>
-    }
-    {
-      props.onButtonClickEdit &&
-      <Button
-        className="float-right mr-2"
-        color="info"
-        type="button"
-        onClick={props.onButtonClickEdit}>{props.buttonTextEdit}</Button> 
-    }
-  </div>
+  <Row className="mb-3">
+    <Col md="8">
+      <h1 className="float-left">{props.headerText}</h1>
+    </Col>
+    <Col md="2" xs="6">
+      {
+        props.onButtonClickEdit &&
+        <Button
+          color="info"
+          type="button"
+          onClick={props.onButtonClickEdit}
+          block>{props.buttonTextEdit}</Button> 
+      }
+    </Col>
+    <Col md="2" xs="6">
+      {
+        props.onButtonClickNew &&
+        <Button
+          color="primary"
+          type="button"
+          onClick={props.onButtonClickNew}
+          block>{props.buttonTextNew}</Button>
+      }
+    </Col>
+  </Row>
 );
--- src/pages/PhaseDetail/PracticeForm.jsx
+++ src/pages/PhaseDetail/PracticeForm.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSave, faTimes } from '@fortawesome/free-solid-svg-icons'
 import {
+  Row, Col,
   Form,
   FormGroup,
   Input,
@@ -14,42 +17,58 @@ const TYPE_OPTIONS = [
 
 export default (props) => {
   return (
-    <Form className="mb-3" onSubmit={props.onSubmit} inline>
-      <FormGroup>
-        <Input
-          className="mr-2"
-          type="text"
-          name="practice-content"
-          id="practice-content"
-          placeholder="Write some..."
-          value={props.value.content}
-          required
-          onChange={props.onChange('content')}
-        />
-      </FormGroup>
-      <FormGroup>
-        <Input
-          className="mr-2"
-          type="select"
-          name="practice-type"
-          id="practice-type"
-          value={props.value.type}
-          required
-          onChange={props.onChange('type')}
-        >
-          {
-            TYPE_OPTIONS.map((opt, index) => <option key={Date.now() + index} value={opt.value}>{opt.text}</option>)
-          }
-        </Input>
-      </FormGroup>
-      <Button
-        className="mr-2"
-        color="secondary"
-        onClick={props.onCancelClick}
-        type="button">Cancel</Button>
-      <Button
-        color="success"
-        type="submit">Save</Button>
-    </Form>
+    <Row>
+      <Form className="mb-3 w-100" onSubmit={props.onSubmit} inline>
+        <Col md="8" xs="12">
+          <FormGroup>
+            <Input
+              className="mr-2 w-100"
+              type="text"
+              name="practice-content"
+              id="practice-content"
+              placeholder="Write some..."
+              value={props.value.content}
+              required
+              onChange={props.onChange('content')}
+            />
+          </FormGroup>
+        </Col>
+        <Col md="2" xs="12">
+          <FormGroup>
+            <Input
+              className="mr-2 w-100"
+              type="select"
+              name="practice-type"
+              id="practice-type"
+              value={props.value.type}
+              required
+              onChange={props.onChange('type')}
+            >
+              {
+                TYPE_OPTIONS.map((opt, index) => <option key={Date.now() + index} value={opt.value}>{opt.text}</option>)
+              }
+            </Input>
+          </FormGroup>
+        </Col>
+        <Col md="2" xs="12">
+          <Row>
+            <Col md="6" xs="6">
+              <Button
+                className="mr-2"
+                color="secondary"
+                onClick={props.onCancelClick}
+                type="button"
+                block><FontAwesomeIcon icon={faTimes}/></Button>
+            </Col>
+            <Col md="6" xs="6">
+              <Button
+                color="success"
+                type="submit"
+                block><FontAwesomeIcon icon={faSave}/></Button>
+            </Col>
+          </Row>
+        </Col>
+      </Form>
+    </Row>
   );
 };
